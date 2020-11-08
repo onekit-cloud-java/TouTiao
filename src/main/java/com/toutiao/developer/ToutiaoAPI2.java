2e1d4d855bde3ffbc703c8cab8322c4ff59b1a8e
your name <your email>
2020-11-08 09:26:37 +0800

    .

--- src/main/java/com/toutiao/developer/ToutiaoAPI2.java
+++ src/main/java/com/toutiao/developer/ToutiaoAPI2.java
@@ -4,7 +4,7 @@ package com.toutiao.developer;
 import java.util.List;
 
 public abstract class ToutiaoAPI2 {
-    public static class errCode extends Exception{
+    public static class errCode extends Exception {
         private String error_id;
         private int code;
         private String message;
@@ -44,45 +44,87 @@ public abstract class ToutiaoAPI2 {
     }
 
     ////////////
-    public static class tags$text$antidirt_body{
+    public static class tags$text$antidirt_body {
+        public static class Task {
+            public String getContent() {
+                return content;
+            }
 
-        public List<String> tasks;
+            public void setContent(String content) {
+                this.content = content;
+            }
 
-        public List<String> getTasks() {
+            private String content;
+        }
+
+        public List<Task> tasks;
+
+        public List<Task> getTasks() {
             return tasks;
         }
 
-        public void setTasks(List<String> tasks) {
+        public void setTasks(List<Task> tasks) {
             this.tasks = tasks;
         }
-
-        public String getContent() {
-            return content;
-        }
-
-        public void setContent(String content) {
-            this.content = content;
-        }
-
-        public  String content;
     }
-    public abstract tags$text$antidirt_response tags$text$antidirt(
-             String X_Token,tags$text$antidirt_body body)throws errCode;
 
+    public abstract tags$text$antidirt_response tags$text$antidirt(
+            String X_Token, tags$text$antidirt_body body) throws errCode;
 
 
     public static class tags$text$antidirt_response {
+        public static class Data {
+
+            private int code;
+            private String msg;
+            private String data_id;
+            private String task_id;
+            private List<Predict> predicts;
+
+            public int getCode() {
+                return code;
+            }
+
+            public void setCode(int code) {
+                this.code = code;
+            }
+
+            public String getMsg() {
+                return msg;
+            }
+
+            public void setMsg(String msg) {
+                this.msg = msg;
+            }
+
+            public String getData_id() {
+                return data_id;
+            }
+
+            public void setData_id(String data_id) {
+                this.data_id = data_id;
+            }
+
+            public String getTask_id() {
+                return task_id;
+            }
+
+            public void setTask_id(String task_id) {
+                this.task_id = task_id;
+            }
+
+            public List<Predict> getPredicts() {
+                return predicts;
+            }
+
+            public void setPredicts(List<Predict> predicts) {
+                this.predicts = predicts;
+            }
+
+        }
+
         private String log_id;
         private String data;
-        private int code;
-        private String msg;
-        private String data_id;
-        private String task_id;
-        private List<Predict> predicts;
-        private String target;
-        private String model_name;
-        private int prob;
-        private boolean hit;
 
         public String getLog_id() {
             return log_id;
@@ -99,82 +141,11 @@ public abstract class ToutiaoAPI2 {
         public void setData(String data) {
             this.data = data;
         }
-
-        public int getCode() {
-            return code;
-        }
-
-        public void setCode(int code) {
-            this.code = code;
-        }
-
-        public String getMsg() {
-            return msg;
-        }
-
-        public void setMsg(String msg) {
-            this.msg = msg;
-        }
-
-        public String getData_id() {
-            return data_id;
-        }
-
-        public void setData_id(String data_id) {
-            this.data_id = data_id;
-        }
-
-        public String getTask_id() {
-            return task_id;
-        }
-
-        public void setTask_id(String task_id) {
-            this.task_id = task_id;
-        }
-
-        public List<Predict> getPredicts() {
-            return predicts;
-        }
-
-        public void setPredicts(List<Predict> predicts) {
-            this.predicts = predicts;
-        }
-
-        public String getTarget() {
-            return target;
-        }
-
-        public void setTarget(String target) {
-            this.target = target;
-        }
-
-        public String getModel_name() {
-            return model_name;
-        }
-
-        public void setModel_name(String model_name) {
-            this.model_name = model_name;
-        }
-
-        public int getProb() {
-            return prob;
-        }
-
-        public void setProb(int prob) {
-            this.prob = prob;
-        }
-
-        public boolean isHit() {
-            return hit;
-        }
-
-        public void setHit(boolean hit) {
-            this.hit = hit;
-        }
     }
+
     ////////////////////////
-    public static class tags$image_body{
-        public static class tags$image_body_task{
+    public static class tags$image_body {
+        public static class tags$image_body_task {
             public String image;
 
             public String getImage() {
@@ -184,10 +155,21 @@ public abstract class ToutiaoAPI2 {
             public void setImage(String image) {
                 this.image = image;
             }
+
+            public String getImage_data() {
+                return image_data;
+            }
+
+            public void setImage_data(String image_data) {
+                this.image_data = image_data;
+            }
+
+            private String image_data;
         }
-        private    List<String> targets;
-        private   List<tags$image_body_task> tasks;
-        private  String image;
+
+        private List<String> targets;
+        private List<tags$image_body_task> tasks;
+        private String image;
 
         public List<String> getTargets() {
             return targets;
@@ -205,41 +187,64 @@ public abstract class ToutiaoAPI2 {
             this.tasks = tasks;
         }
 
-        public String getImage() {
-            return image;
-        }
-
-        public void setImage(String image) {
-            this.image = image;
-        }
-
-        public String getImage_data() {
-            return image_data;
-        }
-
-        public void setImage_data(String image_data) {
-            this.image_data = image_data;
-        }
-
-        private String image_data;
     }
-    public abstract tags$image_response tags$image(String X_Token,tags$image_body body)throws errCode;
 
+    public abstract tags$image_response tags$image(String X_Token, tags$image_body body) throws errCode;
 
 
     public static class tags$image_response {
+        public static class Data {
+            private long code;
+            private String msg;
+            private String data_id;
+            private String task_id;
+            private List<Predict> predicts;
+
+            public long getCode() {
+                return code;
+            }
+
+            public void setCode(long code) {
+                this.code = code;
+            }
+
+            public String getMsg() {
+                return msg;
+            }
+
+            public void setMsg(String msg) {
+                this.msg = msg;
+            }
+
+            public String getData_id() {
+                return data_id;
+            }
+
+            public void setData_id(String data_id) {
+                this.data_id = data_id;
+            }
+
+            public String getTask_id() {
+                return task_id;
+            }
+
+            public void setTask_id(String task_id) {
+                this.task_id = task_id;
+            }
+
+            public List<Predict> getPredicts() {
+                return predicts;
+            }
+
+            public void setPredicts(List<Predict> predicts) {
+                this.predicts = predicts;
+            }
+
+        }
 
         private String log_id;
         private String data;
-        private long code;
-        private String msg;
-        private String data_id;
-        private String task_id;
-        private List<Predict> predicts;
-        private String target;
-        private String model_name;
-        private int prob;
-        private boolean hit;
+
 
         public String getLog_id() {
             return log_id;
@@ -257,76 +262,5 @@ public abstract class ToutiaoAPI2 {
             this.data = data;
         }
 
-        public long getCode() {
-            return code;
-        }
-
-        public void setCode(long code) {
-            this.code = code;
-        }
-
-        public String getMsg() {
-            return msg;
-        }
-
-        public void setMsg(String msg) {
-            this.msg = msg;
-        }
-
-        public String getData_id() {
-            return data_id;
-        }
-
-        public void setData_id(String data_id) {
-            this.data_id = data_id;
-        }
-
-        public String getTask_id() {
-            return task_id;
-        }
-
-        public void setTask_id(String task_id) {
-            this.task_id = task_id;
-        }
-
-        public List<Predict> getPredicts() {
-            return predicts;
-        }
-
-        public void setPredicts(List<Predict> predicts) {
-            this.predicts = predicts;
-        }
-
-        public String getTarget() {
-            return target;
-        }
-
-        public void setTarget(String target) {
-            this.target = target;
-        }
-
-        public String getModel_name() {
-            return model_name;
-        }
-
-        public void setModel_name(String model_name) {
-            this.model_name = model_name;
-        }
-
-        public int getProb() {
-            return prob;
-        }
-
-        public void setProb(int prob) {
-            this.prob = prob;
-        }
-
-        public boolean isHit() {
-            return hit;
-        }
-
-        public void setHit(boolean hit) {
-            this.hit = hit;
-        }
     }
 }
