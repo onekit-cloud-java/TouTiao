b7d54cb4f3030ae8bd03cf89b89adeb2914133c0
your name <your email>
2020-11-15 20:47:05 +0800

    .

--- src/main/java/com/toutiao/developer/ToutiaoAPI.java
+++ src/main/java/com/toutiao/developer/ToutiaoAPI.java
@@ -1,11 +1,24 @@
 package com.toutiao.developer;
 
+import cn.onekit.thekit.SIGN;
 import com.toutiao.developer.entity.*;
 
 public interface ToutiaoAPI {
-
-     String _signBody(String sig_method, String session_key, String data) throws Exception;
-     String _signRaw( String rawData,String session_key) throws Exception;
+     public default String _signBody(String sig_method, String session_key, String body) throws Exception
+    {
+        SIGN.Method method;
+        switch (sig_method) {
+            case "hmac_sha256":
+                method = SIGN.Method.HMACSHA256;
+                break;
+            default:
+                throw new Exception(sig_method);
+        }
+        return new SIGN(method).sign(session_key, body);
+    }
+    public default String _signRaw( String rawData,String session_key) throws Exception{
+        return new SIGN(SIGN.Method.SHA1).sign(rawData+session_key);
+    }
 
     apps__token_response apps__token(
             String appid, String secret,
