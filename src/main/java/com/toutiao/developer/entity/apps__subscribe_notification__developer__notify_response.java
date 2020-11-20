24ce758d689c2d28008a1abe9e418e0f6242e73e
1093357043 <73806828+1093357043@users.noreply.github.com>
2020-11-20 17:22:03 +0800

    .

--- src/main/java/com/toutiao/developer/entity/apps__subscribe_notification__developer__notify_response.java
+++ src/main/java/com/toutiao/developer/entity/apps__subscribe_notification__developer__notify_response.java
@@ -3,6 +3,15 @@ package com.toutiao.developer.entity;
 
 public  class apps__subscribe_notification__developer__notify_response {
     private int err_no;
+    private String err_tips;
+
+    public String getErr_tips() {
+        return err_tips;
+    }
+
+    public void setErr_tips(String err_tips) {
+        this.err_tips = err_tips;
+    }
 
     public int getErr_no() {
         return err_no;
